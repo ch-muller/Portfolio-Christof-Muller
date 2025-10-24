500feb73147f84a16ea19a344ef7a22c8d1987dc
Christof Müller <muller62@pt.lu>
2025-10-24 21:10:46 +0200

    adding an api file

---
+++ api/vote.js
@@ -0,0 +1,18 @@
+// api/vote.js
+export default async function handler(req, res) {
+  global.votes = global.votes || { yes: 0, maybe: 0, no: 0 };
+
+  if (req.method === 'POST') {
+    const { option } = req.body;
+    if (['yes', 'maybe', 'no'].includes(option)) {
+      global.votes[option]++;
+    }
+    return res.status(200).json({ success: true, votes: global.votes });
+  }
+
+  if (req.method === 'GET') {
+    return res.status(200).json({ votes: global.votes });
+  }
+
+  res.status(405).json({ message: 'Method not allowed' });
+}
